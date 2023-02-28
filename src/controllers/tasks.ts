254f9822f6d9da9b4cdf300f4dcdd21dd8103736
awhuiyun <117333975+awhuiyun@users.noreply.github.com>
2023-03-01 07:26:49 +0800

    Added updateExistingTask controller

--- src/controllers/tasks.ts
+++ src/controllers/tasks.ts
@@ -127,27 +127,53 @@ function createNewTask(req: Request, res: Response) {
 }
 
 // Function to update existing task
+type UpdateExistingTaskPayload = {
+  user_id: string;
+  task_id: string;
+  task_name: string;
+  target_num_of_sessions: string;
+  is_completed: boolean;
+  category_name?: string;
+  category_colour?: string;
+};
+
 function updateExistingTask(req: Request, res: Response) {
   try {
-    // const { user_id, yesterday_year, yesterday_month, yesterday_day } =
-    //   req.body as GetIncompleteTasksPayload;
-    // db.query(
-    //   "SELECT tasks.task_name, tasks_sessions.date_of_session, tasks_sessions.number_of_minutes FROM tasks_sessions JOIN tasks ON tasks_sessions.task_id = tasks.id WHERE tasks.user_id = (?) AND YEAR(tasks_sessions.date_of_session) = (?)",
-    //   [user_id],
-    //   (error, result) => {
-    //     if (error) {
-    //       console.log(error);
-    //     } else {
-    //       res.send(result);
-    //     }
-    //   }
-    // );
+    const {
+      user_id,
+      task_id,
+      task_name,
+      target_num_of_sessions,
+      is_completed,
+      category_name,
+      category_colour,
+    } = req.body as UpdateExistingTaskPayload;
+
+    db.query(
+      "UPDATE tasks SET task_name = (?), target_num_of_sessions= (?), is_completed= (?), category_name= (?), category_colour= (?) WHERE user_id= (?) AND id=(?)",
+      [
+        task_name,
+        target_num_of_sessions,
+        is_completed,
+        category_name,
+        category_colour,
+        user_id,
+        task_id,
+      ],
+      (error, result) => {
+        if (error) {
+          console.log(error);
+        } else {
+          res.send("Existing task successfully updated!");
+        }
+      }
+    );
   } catch (error) {
-    // console.error(" POST /tasks/incomplete-tasks", error);
-    // return res.status(400).json({
-    //   status: "error",
-    //   message: "request to get incomplete tasks failed",
-    // });
+    console.error(" PATCH /tasks/update", error);
+    return res.status(400).json({
+      status: "error",
+      message: "request to update existing task failed",
+    });
   }
 }
 
