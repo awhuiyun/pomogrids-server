ae2a8adad3b9fc9022fe6d5182d2008f5ee3b0a3
awhuiyun <117333975+awhuiyun@users.noreply.github.com>
2023-03-07 17:19:57 +0800

    Edited schema file

--- src/models/schema.sql
+++ src/models/schema.sql
@@ -8,9 +8,10 @@ CREATE TABLE users (
 );
 
 CREATE TABLE tasks (
-    id SMALLINT PRIMARY KEY AUTO_INCREMENT,
+    id VARCHAR(255) PRIMARY KEY,
     task_name VARCHAR(255) NOT NULL,
     target_num_of_sessions SMALLINT NOT NULL,
+    is_archived BOOLEAN NOT NULL,
     category_name VARCHAR(255),
     category_colour VARCHAR(255),
     user_id VARCHAR(255) NOT NULL,
@@ -19,10 +20,10 @@ CREATE TABLE tasks (
 
 CREATE TABLE tasks_sessions (
     session_id SMALLINT PRIMARY KEY AUTO_INCREMENT,
-    task_id SMALLINT NOT NULL,
+    task_id VARCHAR(255) NOT NULL,
     number_of_sessions SMALLINT NOT NULL,
     number_of_minutes SMALLINT NOT NULL,
-    date_of_session STRING NOT NULL,
+    date_of_session VARCHAR(45) NOT NULL,
     CONSTRAINT fk_tasks FOREIGN KEY(task_id) REFERENCES tasks(id)
 );
 
@@ -34,17 +35,7 @@ CREATE TABLE settings (
     number_of_sessions_in_a_cycle SMALLINT NOT NULL, 
     alarm_ringtone VARCHAR(255) NOT NULL,
     alarm_volume DECIMAL(2, 1) NOT NULL,
+    week_start VARCHAR(45) NOT NULL,
     CONSTRAINT fk_users_settings FOREIGN KEY(user_id) REFERENCES users(id)
 );
 
-INSERT INTO users (id, email, tier) VALUES (UUID(), 'awhuiyun@gmail.com', 'basic');
-INSERT INTO users (id, email, tier) VALUES (UUID(), 'raymoay@gmail.com', 'premium');
-
-INSERT INTO tasks (task_name, target_num_of_sessions, completed_num_of_sessions, is_completed, user_id) VALUES ("Learn typescript and MySQL", 5, 2, false, "ca00d32a-b740-11ed-8877-0242ac110002")
-INSERT INTO tasks (task_name, target_num_of_sessions, completed_num_of_sessions, is_completed, user_id) VALUES ("Build todogrids", 3, 2, false, "ca01723a-b740-11ed-8877-0242ac110002")
-
-INSERT INTO tasks_sessions (task_id, number_of_sessions, number_of_minutes) VALUES (1, 2, 120);
-INSERT INTO tasks_sessions (task_id, number_of_sessions, number_of_minutes) VALUES (3, 2, 180);
-
-INSERT INTO settings (user_id, pomodoro_minutes, short_break_minutes, long_break_minutes, number_of_sessions_in_a_cycle, alarm_ringtone, alarm_volume) VALUES ("ca00d32a-b740-11ed-8877-0242ac110002", 25, 5, 15, 4, "default", 0.5);
-INSERT INTO settings (user_id, pomodoro_minutes, short_break_minutes, long_break_minutes, number_of_sessions_in_a_cycle, alarm_ringtone, alarm_volume) VALUES ("ca01723a-b740-11ed-8877-0242ac110002", 25, 5, 15, 4, "default", 0.5);
